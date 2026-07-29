4ed4927c81e457aef1e0a4da92134490efcc0aac
Himanshu Kumar <146649978+himankumar08@users.noreply.github.com>
2026-07-30 04:53:16 +0530

    Create Demo_py.py for GitHub Actions linting
    Added a demo Python file to check GitHub Actions for linting.

---
+++ Demo_py.py
@@ -0,0 +1,10 @@
+"""This is and demo python file for checking github action where we are using github action with ephermal runner to check python syntax ( linting )"""
+def intro():
+  name = "Himanshu Kumar"
+  age = 22
+  usecase = "Linting a pyton file using github action.IF you see this message this action is succesfull !!!"
+  print(f"Hii {name} here !!!, I am {age} year old." + usecase)
+  return "Execution completed !!"
+
+print(intro())
+  
